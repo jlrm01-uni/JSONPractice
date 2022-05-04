1b0606ff421ae3801bdf58055955aaacfb9dccaf
Jorge Rodriguez <jorge.rodriguez@guayama.inter.edu>
2022-05-04 09:30:15 -0400

    added stuff

--- app/src/main/java/com/example/jsonpractice/MainActivity.kt
+++ app/src/main/java/com/example/jsonpractice/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.jsonpractice
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.jsonpractice.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import java.io.File
@@ -10,6 +12,7 @@ import java.io.File
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     var notesCollection: NotesCollection = NotesCollection()
+    val saveFilename = "all_notes.json"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,27 +24,70 @@ class MainActivity : AppCompatActivity() {
             addNote()
         }
 
+        binding.deleteEverythingButton.setOnClickListener {
+            showDeleteEverythingDialog()
+        }
+
+        loadNotes()
+        displayNotes()
+    }
+
+    private fun showDeleteEverythingDialog() {
+        val myDialog = AlertDialog.Builder(this)
+
+        myDialog.setTitle("DELETE EVERYTHING?!")
+        myDialog.setMessage("This is irreversible, are you really really reaaaaally sure?")
+
+        myDialog.setPositiveButton("Get rid of it!", DialogInterface.OnClickListener { dialogInterface, i ->
+            deleteEverything()
+        })
+
+        myDialog.setNegativeButton("Noooooooo", DialogInterface.OnClickListener { dialogInterface, i ->
+            // Nothing to do here
+        })
+
+        myDialog.show()
     }
 
     fun loadNotes() {
+        val file = File(filesDir, saveFilename)
 
+        if (!file.exists()) {
+            Toast.makeText(this, "No notes saved yet.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val string = file.readText()
+
+        val loadedCollection = Gson().fromJson(string, NotesCollection::class.java)
+
+        notesCollection = loadedCollection
     }
 
     fun saveNotes() {
         val jsonString = Gson().toJson(notesCollection)
 
-        val file = File(filesDir, "all_notes.json")
+        val file = File(filesDir, saveFilename)
         file.writeText(jsonString)
 
         Toast.makeText(this, "Notes Saved!", Toast.LENGTH_LONG).show()
     }
 
     fun deleteEverything() {
+        val file = File(filesDir, saveFilename)
 
+        if (file.exists()) {
+            file.delete()
+
+            Toast.makeText(this, "Everything is gone :(", Toast.LENGTH_LONG).show()
+
+            notesCollection.clearNotes()
+            displayNotes()
+        }
     }
 
     fun displayNotes() {
-
+        binding.allNotesTextView.text = notesCollection.toString()
     }
 
     fun addNote() {
@@ -65,6 +111,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clearFields() {
-
+        binding.subjectEditText.text.clear()
+        binding.contentEditText.text.clear()
     }
+
 }
